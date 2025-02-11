ef571bb224724b0c3d95b8076a4658425e954787
brunoss-96 <bruno.ss318@gmail.com>
2025-02-11 20:40:25 -0300

    javascript 	modified:   javascript/index.html 	modified:   javascript/index.js

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+console.log("Meu código está rodando!");
+
+let firstName = "Bruno";        
+let lastName = "Silva";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let age = 29;
+console.log(age);
+
+
+
+
+
+
+
